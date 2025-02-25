805c141e3333dcc726b40cbd72f8be3cc9d89d4a
kobeay <rhaudwns4981@naver.com>
2025-02-25 23:34:23 +0900

    ✨ 회원가입 요청 DTO

---
+++ src/main/java/com/github/kobeay/login_server/dto/RegisterRequest.java
@@ -0,0 +1,20 @@
+package com.github.kobeay.login_server.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class RegisterRequest {
+    @NotBlank
+    @Schema(description = "유저 아이디")
+    private String username;
+
+    @NotBlank
+    @Schema(description = "비밀번호")
+    private String password;
+}
