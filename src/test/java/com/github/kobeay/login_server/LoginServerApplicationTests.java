449e39237212817a265cc14713dd63f1af0ea35d
MaiT <maitmus@gmail.com>
2025-03-10 03:47:15 +0900

    fix: 테스트 파일 최적화

--- src/test/java/com/github/kobeay/login_server/LoginServerApplicationTests.java
+++ src/test/java/com/github/kobeay/login_server/LoginServerApplicationTests.java
@@ -2,12 +2,19 @@ package com.github.kobeay.login_server;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 
 @SpringBootTest
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@TestPropertySource(properties = {
+        "spring.datasource.url=jdbc:h2:mem:testdb",
+        "spring.datasource.driver-class-name=org.h2.Driver",
+        "spring.datasource.username=sa",
+        "spring.datasource.password=",
+        "spring.jpa.database-platform=org.hibernate.dialect.H2Dialect",
+        "spring.jpa.hibernate.ddl-auto=create-drop"
+})
 class LoginServerApplicationTests {
     @DisplayName("애플리케이션 구동 테스트")
 	@Test
